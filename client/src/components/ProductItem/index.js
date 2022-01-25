3470651554c6239f7f91b4138255b29c7cc3b860
TJ James <jamestw13@gmail.com>
2022-01-25 15:21:17 -0600

    added shopping carts input value add remove and update functionality

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -7,11 +7,23 @@ import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
 
+  const { cart } = state;
+
   const addToCart = () => {
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...item, purchaseQuantity: 1 },
-    });
+    const itemInCart = cart.find(cartItem => cartItem._id === _id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 },
+      });
+    }
   };
 
   const { image, name, _id, price, quantity } = item;
