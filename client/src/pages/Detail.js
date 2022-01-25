5342dfbb5bbb284fbb99beab13601eb9748db611
TJ James <jamestw13@gmail.com>
2022-01-25 14:36:03 -0600

    created Cart and CartItem components and added them to Home.js and Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -8,6 +8,8 @@ import spinner from '../assets/spinner.gif';
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
 
+import Cart from '../components/Cart';
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
@@ -48,6 +50,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
