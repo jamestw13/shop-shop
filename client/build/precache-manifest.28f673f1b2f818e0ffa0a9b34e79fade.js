48f6b47191198b3f7dd87eca9db4a7740edd9404
TJ James <jamestw13@gmail.com>
2022-01-26 15:13:56 -0600

    ran build script

---
+++ client/build/precache-manifest.28f673f1b2f818e0ffa0a9b34e79fade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf2d1663c3547b269d26b415def8178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3eba76b33d5f6ca0ec",
+    "url": "/static/css/main.e3b24089.chunk.css"
+  },
+  {
+    "revision": "4f4541aaf0010e929975",
+    "url": "/static/js/2.e9ad615d.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e9ad615d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3eba76b33d5f6ca0ec",
+    "url": "/static/js/main.df500fce.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
